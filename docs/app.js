c08382a868af4aeeaeaa6db208d5f9d2523ba556
Aranarora <aranarora007@gmail.com>
2023-01-20 18:18:02 -0800

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -1,26 +1,16 @@
 "use strict";
 
-function calculateAnnualRate( monthlyRate){
-    let annualrate = monthlyRate * 12;
-    return annualrate;
-}
-
 function selectPlan(planName, monthlyRate){
-    let annualrate = calculateAnnualRate(monthlyRate);
 
-    let planNameElement = document.getElementById("planname");
-    planNameElement.innerHTML = planName;
-
-    let monthlyRateElement = document.getElementById("monthlyrate");
-    monthlyRateElement.innerHTML = '$' + monthlyRate + '/mo';
-
-    let annualRateElement = document.getElementById("annualrate");
-    annualRateElement.innerHTML = '$' + annualrate + '/yr';
-
+    let planVale = document.getElementById("planname");
+    planVale.innerHTML = planName;
+    let monthlyRateVale = document.getElementById("monthlyrate");
+    monthlyRateVale.innerHTML = "$" + monthlyRate + "/mo";
+    let annualRateValue = document.getElementById("annualrate");
+    annualRateValue.innerHTML = "$" + monthlyRate * 12 + "/yr";
     let today = new Date();
     let nextYear = new Date();
     nextYear.setFullYear (today.getFullYear() + 1);
-
-    let gooduntilElement = document.getElementById("gooduntil");
-    gooduntilElement.innerHTML = nextYear.toDateString();
+    let gooduntilValue = document.getElementById("gooduntil");
+    gooduntilValue.innerHTML = nextYear.toDateString;
 }
