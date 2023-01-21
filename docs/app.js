ffee1dbd96e493276e66f22df671722a70b3df98
Aranarora <aranarora007@gmail.com>
2023-01-20 18:00:34 -0800

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -1,16 +1,24 @@
 "use strict";
+function calculateAnnualRate( monthlyRate){
+    let annualrate = monthlyRate * 12;
+    return annualrate;
+}
 
 function selectPlan(planName, monthlyRate){
 
     let planVale = document.getElementById("planname");
     planVale.innerHTML = planName;
+
     let monthlyRateVale = document.getElementById("monthlyrate");
     monthlyRateVale.innerHTML = "$" + monthlyRate + "/mo";
+
     let annualRateValue = document.getElementById("annualrate");
-    annualRateValue.innerHTML = "$" + monthlyRate * 12 + "/yr";
+    annualRateValue.innerHTML = "$" + annualrate + "/yr";
+
     let todayDate = new Date();
     let nextYear = new Date();
     nextYear.setFullYear = todayDate.getFullYear + 1;
+
     let gooduntilValue = document.getElementById("gooduntil");
     gooduntilValue.innerHTML = nextYear.toDateString();
 }
